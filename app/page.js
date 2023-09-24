d0633b56b871d5b49b1a438dd8e8e976e3284265
Dr-savant21 <israelnosakhare21@gmail.com>
2023-09-24 10:17:48 +0100

    footer.js added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Navbar from './components/Navbar'
+import Footer from './components/footer'
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
       
+      <Footer />
     </main>
   )
 }
