f6f84b25824f18f4a2f8e0fde70e13a86e115b47
Abiodun Adekunle <94719889+Abiodun001-world@users.noreply.github.com>
2023-10-05 02:14:53 +0000

    deleted master file in main the branch once its merged

--- app/sign-in/[[...sign-in]]/page.js
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
- 
-export default function Page() {
-  return <>
-  <div className="flex justify-center m-10">
-    <SignIn />
-  </div>
-  </> ;
-}
--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-   Head: {
-        title: 'Techbook9ja',
-      },
-    };
-
-module.exports = nextConfig
